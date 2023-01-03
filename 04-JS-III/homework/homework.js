0f1df10c3879f66ec6c26b3316f8da858d718a50
Napa <juan.urquiza.ar@hotmail.com>
2023-01-03 20:36:03 +0100

    Mejoras en Homework 3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -189,7 +189,7 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-  var vM = [];
+  const vM = [];
   for (var i= 0; i< array.length; i++){
     if (array[i].includes('Enero') || array[i].includes('Marzo') || array[i].includes('Noviembre')  ) vM.push(array [i])
   }
